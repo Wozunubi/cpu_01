55877dd86c1751ab16bc46463fd8376e6b53f416
George Fan <georgefan112358@gmail.com>
2025-06-17 21:03:05 -0400

    Added thorough alu tests

--- tb/tb_alu.sv
+++ tb/tb_alu.sv
@@ -22,9 +22,35 @@ module tb_alu;
   );
 
   initial begin
-    in1 = 8'h0A;
-    in2 = 8'h05;
-    alu_op = ALU_ADD;
-    #1;
+    // ADD
+    in1 = 8'h01; in2 = 8'h01; alu_op = ALU_ADD; #1;
+
+    in1 = 8'hFF; in2 = 8'h01; alu_op = ALU_ADD; #1;
+
+    in1 = 8'h7F; in2 = 8'h01; alu_op = ALU_ADD; #1;
+
+    in1 = 8'h80; in2 = 8'h80; alu_op = ALU_ADD; #1;
+
+    // SUB
+    in1 = 8'h05; in2 = 8'h03; alu_op = ALU_SUB; #1;
+
+    in1 = 8'h00; in2 = 8'h01; alu_op = ALU_SUB; #1;
+
+    in1 = 8'h80; in2 = 8'h01; alu_op = ALU_SUB; #1;
+
+    in1 = 8'h01; in2 = 8'h01; alu_op = ALU_SUB; #1;
+
+    // Logic/Shifts
+    in1 = 8'hF0; in2 = 8'h0F; alu_op = ALU_AND; #1;
+
+    in1 = 8'hF0; in2 = 8'h0F; alu_op = ALU_OR; #1;
+
+    in1 = 8'hAA; in2 = 8'h55; alu_op = ALU_XOR; #1;
+
+    in1 = 8'h01; in2 = '0; alu_op = ALU_SLL; #1;
+
+    in1 = 8'h02; in2 = '0; alu_op = ALU_SRL; #1;
+
+    in1 = 8'h80; in2 = '0; alu_op = ALU_SRA; #1;
   end
 endmodule
