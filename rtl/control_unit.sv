5a5ce6038e2c28d5f67c1b911e59b4d2c610e9d3
George Fan <georgefan112358@gmail.com>
2025-06-23 23:26:30 -0400

    Defined states and control signals

--- rtl/control_unit.sv
+++ rtl/control_unit.sv
@@ -1,17 +1,58 @@
 module control_unit #(
-  parameter INSTRUCTION_WIDTH = 16
-  parameter STATE_WIDTH = 5
+  parameter INSTRUCTION_WIDTH = 16,
+  parameter STATE_WIDTH = 4
 ) (
   input logic [INSTRUCTION_WIDTH-1:0] instruct,
-  input logic [STATE_WIDTH-1:0] Y,
-  output logic [STATE_WIDTH-1:0] y,
+  input alu_flags_t flags,
+  input logic clk,
+  input logic resetn,
+  input state_t [STATE_WIDTH-1:0] y,
+  output state_t [STATE_WIDTH-1:0] Y,
   output ctrl_sig_t sigs
 );
 
   import defs_pkg::*;
 
-  always_comb begin
+  always_ff @(posedge clk) begin
+    if (resetn) begin
+      y <= '0;
+      // set default sigs
+    end
 
+    case (y)
+      STATE_FETCH: begin
+      end
+      STATE_DECODE: begin
+      end
+      STATE_WB_LI: begin
+      end
+      STATE_EXEC_ADDI: begin
+      end
+      STATE_WB_ADDI: begin
+      end
+      STATE_EXEC_LW: begin
+      end
+      STATE_MEM_LW: begin
+      end
+      STATE_WB_LW: begin
+      end
+      STATE_EXEC_SW: begin
+      end
+      STATE_MEM_SW: begin
+      end
+      STATE_EXEC_ALU: begin
+      end
+      STATE_WB_ALU: begin
+      end
+      STATE_EXEC_LINK: begin
+      end
+      STATE_EXEC_JMP: begin
+      end
+      STATE_EXEC_JPR: begin
+      end
+      STATE_EXEC_BRH: begin
+      end
+    endcase
   end
 
 endmodule
--- rtl/defs_pkg.sv
+++ rtl/defs_pkg.sv
@@ -17,7 +17,37 @@ package defs_pkg;
     logic overflow;
   } alu_flags_t;
 
+  typedef enum logic [3:0] {
+    STATE_FETCH = 4'b0000,
+    STATE_DECODE = 4'b0001,
+    STATE_WB_LI = 4'b0010,
+    STATE_EXEC_ADDI = 4'b0011,
+    STATE_WB_ADDI = 4'b0100,
+    STATE_EXEC_LW = 4'b0101,
+    STATE_MEM_LW = 4'b0110,
+    STATE_WB_LW = 4'b0111,
+    STATE_EXEC_SW = 4'b1000,
+    STATE_MEM_SW = 4'b1001,
+    STATE_EXEC_ALU = 4'b1010,
+    STATE_WB_ALU = 4'b1011,
+    STATE_EXEC_LINK = 4'b1100,
+    STATE_EXEC_JMP = 4'b1101,
+    STATE_EXEC_JPR = 4'b1110,
+    STATE_EXEC_BRH = 4'b1111
+  } state_t;
+
   typedef struct packed {
-    logic 
+    logic PC_write;
+    logic ROM_read;
+    logic MEM_read;
+    logic MEM_write;
+    logic RF_write;
+    logic ALU_op;
+    logic FLAG_load;
+    logic MDR_load;
+    logic MAR_load;
+    logic IR_load;
+    logic AB_load;
+    logic ACC_load;
   } ctrl_sig_t;
 endpackage
